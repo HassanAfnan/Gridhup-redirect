9c87d456d7046bd60dea8593162f3c1d875cd705
Afnan Hassan <hassanafnan20@gmail.com>
2022-07-19 19:10:25 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import GridLogo from "./Assets/Gridhup.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={GridLogo} />
+      <p>Congratulations</p>
+      <p>Your account has been created</p>
+      <div>
+        <a href="app://gridhup/login">Finish</a>
+      </div>
     </div>
   );
 }
